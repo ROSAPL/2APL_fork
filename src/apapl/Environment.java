9cd966e52d26c6f571012aaba15c7ae375dd5e1b
Emili Boronat <eboronat@ibecbarcelona.eu>
2014-06-30 18:38:42 +0200

    Convert Environment to abstract class

--- src/apapl/Environment.java
+++ src/apapl/Environment.java
@@ -23,208 +23,206 @@ import apapl.env.exceptions.NoEnvironmentException;
  * Therefore this class can be used to extend and build a custom environment.
  * The 
  */
-public class Environment {
+public abstract class Environment {
+
 	private HashMap<String,String> envParams = new HashMap<String,String>();
-	
 	/**
-     * This method is meant to be overridden by sub-classes. It is invoked when
-     * an agent enters the environment. Note that this method is also invoked each
-     * time an agent is re-compiled.
-     * 
-     * @param name the local name of the specific agent.
-     */
-    protected void addAgent(String name) {
-    };
+	 * Stores for each agent (represented by a string) a listener.
+	 */
+	protected HashMap<String, AgentListener> agents = null;
 
-    /**
-     * This method is invoked when an agent operating in this environment is
-     * removed. Note that this method is also invoked when an agent is
-     * recompiled.
-     * 
-     * @param name the local name of the specific agent.
-     */
-    protected void removeAgent(String name) {
-    };
-	
-	
-	 /**
-     * Stores for each agent (represented by a string) a listener.
-     */
-    protected HashMap<String, AgentListener> agents = null;
 
-    /**
-     * Instantiates the class.
-     */
-    public Environment() {
-        agents = new HashMap<String, AgentListener>();
-    }
-    
-    /**
-     * Throws an event to one or more agents.
-     * 
-     * @param event the event to be thrown
-     * @param receivers the agent listed to receive the event. If no
-     *        no agents are listed, the event will be thrown to all agents.
-     * @throws EnvironmentInterfaceException 
-     */
-    public final void throwEvent(APLFunction e, String... pAgents) {
-        notifyAgents(e, pAgents);
-    }
+	/**
+	 * This method is meant to be overridden by sub-classes. It is invoked when
+	 * an agent enters the environment. Note that this method is also invoked each
+	 * time an agent is re-compiled.
+	 * 
+	 * @param name the local name of the specific agent.
+	 */
+        abstract protected void addAgent(String name);
 
-    /**
-     * Returns the name of this environment. This is equal to the package name.
-     */
-    public final String getName() {
-        String sourceEnv = getClass().getName();
-        return sourceEnv.substring(0, sourceEnv.lastIndexOf("."));
-    }
+	/**
+	 * This method is invoked when an agent operating in this environment is
+	 * removed. Note that this method is also invoked when an agent is
+	 * recompiled.
+	 * 
+	 * @param name the local name of the specific agent.
+	 */
+	 abstract protected void removeAgent(String name);
 
-    /*
-     * 
-     * TODO: change doc
-     * 
-     */
-    public final void registerAgent(String agent, AgentListener listener) {
-        agents.put(agent, listener);
-        addAgent(agent); // call to method that can be overridden by superclass
-    }
 
-    /*
-     * 
-     * TODO: change doc
-     * 
-     */
-    public void deregisterAgent(String agent) throws AgentException {
-        if (agents.containsKey(agent) == false)
-            return;
 
-        agents.remove(agent);
-        removeAgent(agent);
-    }
+	/**
+	 * Instantiates the class.
+	 */
+	public Environment() {
+		agents = new HashMap<String, AgentListener>();
+	}
 
-    /**
-     * Notifies agents about a percept.
-     * 
-     * @param percept is the percept
-     * @param agents is the array of agents that are to be notified about the
-     *        event. If the array is empty, all registered agents will be
-     *        notified. The array has to contain only registered agents.
-     * @throws AgentException is thrown if at least one of the agents in the
-     *         array is not registered.
-     */
-    protected final void notifyAgents(APLFunction msg, String... pAgents)
-    {
-    	try 
-    	{
-	        // send to all registered agents
-	        if (pAgents.length == 0) {
-	            for (String agent : agents.keySet()) {
-	            	agents.get(agent).handleMessage(agent, msg);
-	            }
-	            return;
-	        }
-	
-	        // send to specified agents
-	        for (String agent : pAgents) {
-	
-	            if (!agents.containsKey(agent))
-						throw new EnvironmentInterfaceException("Agent " + agent
-						        + " has not registered to the environment.");
-	
-	            agents.get(agent).handleMessage(agent, msg);
-	        }
-    	} catch (EnvironmentInterfaceException e) {
+	/**
+	 * Throws an event to one or more agents.
+	 * 
+	 * @param event the event to be thrown
+	 * @param receivers the agent listed to receive the event. If no
+	 *        no agents are listed, the event will be thrown to all agents.
+	 * @throws EnvironmentInterfaceException 
+	 */
+	public final void throwEvent(APLFunction e, String... pAgents) {
+		notifyAgents(e, pAgents);
+	}
+
+	/**
+	 * Returns the name of this environment. This is equal to the package name.
+	 */
+	public final String getName() {
+		String sourceEnv = getClass().getName();
+		return sourceEnv.substring(0, sourceEnv.lastIndexOf("."));
+	}
+
+	/*
+	 * 
+	 * TODO: change doc
+	 * 
+	 */
+	public final void registerAgent(String agent, AgentListener listener) {
+		agents.put(agent, listener);
+		addAgent(agent); // call to method that can be overridden by superclass
+	}
+
+	/*
+	 * 
+	 * TODO: change doc
+	 * 
+	 */
+	public void deregisterAgent(String agent) throws AgentException {
+		if (agents.containsKey(agent) == false)
+			return;
+
+		agents.remove(agent);
+		removeAgent(agent);
+	}
+
+	/**
+	 * Notifies agents about a percept.
+	 * 
+	 * @param percept is the percept
+	 * @param agents is the array of agents that are to be notified about the
+	 *        event. If the array is empty, all registered agents will be
+	 *        notified. The array has to contain only registered agents.
+	 * @throws AgentException is thrown if at least one of the agents in the
+	 *         array is not registered.
+	 */
+	protected final void notifyAgents(APLFunction msg, String... pAgents)
+	{
+		try 
+		{
+			// send to all registered agents
+			if (pAgents.length == 0) {
+				for (String agent : agents.keySet()) {
+					agents.get(agent).handleMessage(agent, msg);
+				}
+				return;
+			}
+
+			// send to specified agents
+			for (String agent : pAgents) {
+
+				if (!agents.containsKey(agent))
+					throw new EnvironmentInterfaceException("Agent " + agent
+							+ " has not registered to the environment.");
+
+				agents.get(agent).handleMessage(agent, msg);
+			}
+		} catch (EnvironmentInterfaceException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
 
-    /**
-     * Performs action in the original 2APL environment.
-     */ 
-    public final Term performAction(String agent,
-            APLFunction action) throws ActException,
-            NoEnvironmentException {
+	/**
+	 * Performs action in the original 2APL environment.
+	 */ 
+	public final Term performAction(String agent,
+			APLFunction action) throws ActException,
+	       NoEnvironmentException {
 
-        // unregistered agents cannot act
-        if (!agents.containsKey(agent))
-            throw new ActException("Agent \"" + agent + "\" is not registered.");
+		       // unregistered agents cannot act
+		       if (!agents.containsKey(agent))
+			       throw new ActException("Agent \"" + agent + "\" is not registered.");
 
-        // Parameters expressed using 2APL data types
-        ArrayList<Term> params = action.getParams();
-        
-        // determine class parameters for finding the method
-        // and store the parameters as objects
+		       // Parameters expressed using 2APL data types
+		       ArrayList<Term> params = action.getParams();
 
-        Class<?>[] classParams = new Class[params.size() + 1];
-        classParams[0] = String.class; // entity name
-        
-        for (int a = 0; a < params.size(); a++)
-            classParams[a + 1] = params.get(a).getClass();
+		       // determine class parameters for finding the method
+		       // and store the parameters as objects
 
-        try {
-            // lookup the method
-            Method m = this.getClass().getMethod(action.getName(), classParams);
+		       Class<?>[] classParams = new Class[params.size() + 1];
+		       classParams[0] = String.class; // entity name
 
-            if (Class.forName("apapl.data.Term").isAssignableFrom(
-                    m.getReturnType()) == false)
-                throw new ActException("Wrong return-type");
+		       for (int a = 0; a < params.size(); a++)
+			       classParams[a + 1] = params.get(a).getClass();
 
-            // invoke
-            Object[] objParams = new Object[params.size() + 1];
-            objParams[0] = agent; // agent name
-            for (int a = 0; a < params.size(); a++)
-            	objParams[a + 1] = params.get(a);
+		       try {
+			       // lookup the method
+			       Method m = this.getClass().getMethod(action.getName(), classParams);
 
-            return (Term) m.invoke(this, objParams);
+			       if (Class.forName("apapl.data.Term").isAssignableFrom(
+						       m.getReturnType()) == false)
+				       throw new ActException("Wrong return-type");
 
-        } catch (ClassNotFoundException e) {
-            throw new ActException("Class not found", e);
-        } catch (SecurityException e) {
-            throw new ActException("Security exception", e);
-        } catch (NoSuchMethodException e) {
-            throw new ActException("No such method", e);
-        } catch (IllegalArgumentException e) {
-            throw new ActException("Illegal argument", e);
-        } catch (IllegalAccessException e) {
-            throw new ActException("Illegal access", e);
-        } catch (InvocationTargetException e) {
-            
-            // action has failed -> let fail
-            if (e.getCause() instanceof ExternalActionFailedException)
-                throw new ActException("Execution failed.", (Exception) e
-                        .getCause()); // rethrow
-            
-            else if (e.getCause() instanceof NoEnvironmentException)
-                throw (NoEnvironmentException) e.getCause(); // rethrow
+			       // invoke
+			       Object[] objParams = new Object[params.size() + 1];
+			       objParams[0] = agent; // agent name
+			       for (int a = 0; a < params.size(); a++)
+				       objParams[a + 1] = params.get(a);
 
-            throw new ActException("Invocation target exception", e);
-        }
-    }
-    
-    /**
-     * This method is called when all the environment parameters have been added
-     * to the environment and it has been added to the module as well. It can be
-     * overridden by an Environment developer to determine the point when the
-     * environment has received all necessary information for the APAPLBuilder.
-     * 
-     */
-    public void initialized() {
-    }
-    
-    
-    /*
-     * Environment parameters functionality
-     * 
-     */
-    
+			       return (Term) m.invoke(this, objParams);
+
+		       } catch (ClassNotFoundException e) {
+			       throw new ActException("Class not found", e);
+		       } catch (SecurityException e) {
+			       throw new ActException("Security exception", e);
+		       } catch (NoSuchMethodException e) {
+			       throw new ActException("No such method", e);
+		       } catch (IllegalArgumentException e) {
+			       throw new ActException("Illegal argument", e);
+		       } catch (IllegalAccessException e) {
+			       throw new ActException("Illegal access", e);
+		       } catch (InvocationTargetException e) {
+
+			       // action has failed -> let fail
+			       if (e.getCause() instanceof ExternalActionFailedException)
+				       throw new ActException("Execution failed.", (Exception) e
+						       .getCause()); // rethrow
+
+			       else if (e.getCause() instanceof NoEnvironmentException)
+				       throw (NoEnvironmentException) e.getCause(); // rethrow
+
+			       throw new ActException("Invocation target exception", e);
+		       }
+	       }
+
+	/**
+	 * This method is called when all the environment parameters have been added
+	 * to the environment and it has been added to the module as well. It can be
+	 * overridden by an Environment developer to determine the point when the
+	 * environment has received all necessary information for the APAPLBuilder.
+	 * 
+	 */
+	abstract public void initialized();
+
+
+	/*
+	 * Environment parameters functionality
+	 * 
+	 */
 	void addEnvParameter(String key, String value) {
 		envParams.put(key, value);
 	}
-	
+
 	public HashMap<String, String> getEnvParameters() {
 		return envParams;
 	}
-	
+
 }
